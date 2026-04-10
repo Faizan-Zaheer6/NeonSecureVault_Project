3b44d0de2d45e95ab21a165f221ae188e3bc1584
Faizan-Zaheer6 <faizanfz@gmail.com>
2026-04-11 00:07:07 +0500

    Fixed missing url

--- app/ui.py
+++ app/ui.py
@@ -4,7 +4,7 @@ import pandas as pd
 import streamlit.components.v1 as components
 
 # Backend URL
-BASE_URL = "https://neonsecurevaultprojectfrontend-production.up.railway.app"
+BASE_URL = "https://neonsecurevaultproject-production.up.railway.app"
 
 st.set_page_config(page_title="NeonVault Admin Pro", layout="wide", initial_sidebar_state="collapsed")
 
